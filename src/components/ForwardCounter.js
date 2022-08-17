de62fd5caa6270c02c4bb824d70b24cb27ed8b4f
dlrlseong <leekisung0212@gmail.com>
2022-08-17 13:55:57 +0900

    198 사용자 정의 훅 구성하기

---
+++ src/components/ForwardCounter.js
@@ -0,0 +1,20 @@
+import Card from "./Card";
+import useCounter from "../hooks/useCounter";
+
+const ForwardCounter = () => {
+  const counter = useCounter(true);
+
+  // const [counter, setCounter] = useState(0);
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCounter((prevCounter) => prevCounter + 1);
+  //   }, 1000);
+
+  //   return () => clearInterval(interval);
+  // }, []);
+
+  return <Card>{counter}</Card>;
+};
+
+export default ForwardCounter;
